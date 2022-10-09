4b6b8d76889e66ccb85036289cdb2e00d15aa4f1
Celso Furtado <celsofurtado@terra.com.br>
2022-10-09 17:05:32 -0300

    Ajuste LookAndFeel

--- src/br/senai/sp/jandira/AgendaApp.java
+++ src/br/senai/sp/jandira/AgendaApp.java
@@ -1,13 +1,35 @@
 package br.senai.sp.jandira;
 
 import br.senai.sp.jandira.ui.AgendaAppFrame;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 public class AgendaApp {
+    
+    
 
     public static void main(String[] args) {
-               
-        AgendaAppFrame agenda = new AgendaAppFrame();
-        agenda.setVisible(true);
+        
+        
+
+        try {
+            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+
+            AgendaAppFrame agenda = new AgendaAppFrame();
+            agenda.setVisible(true);
+            
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(AgendaApp.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            Logger.getLogger(AgendaApp.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(AgendaApp.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(AgendaApp.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
     }
 
